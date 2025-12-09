9984c48e521a162d85ae7dd53f5b2c21af82095b
thien <bananakind123@gmail.com>
2025-12-09 18:24:55 +0700

    feat: env files

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,9 @@
+import { TMDB_ACCESS_TOKEN, TMDB_BASE_URL, TMDB_IMAGE_BASE_URL } from '@env';
+
 export const ENV = {
-  TMDB_ACCESS_TOKEN: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjYxZWY3YzAxYWFjMzIzNGQ2MTAzMTVhZDIyMTUyMSIsIm5iZiI6MTc2NTI2NDk1MS43MjMsInN1YiI6IjY5MzdjZTM3NGE4MDM5NWI5MmViMWE3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.we_9zq2Kssy66pydHyTS2lkJZF0JkMEIRvU0p7ws1IU',
-  TMDB_BASE_URL: 'https://api.themoviedb.org/3',
-  TMDB_IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w500',
+  TMDB_ACCESS_TOKEN,
+  TMDB_BASE_URL,
+  TMDB_IMAGE_BASE_URL,
 } as const;
 
 export type EnvType = typeof ENV;
